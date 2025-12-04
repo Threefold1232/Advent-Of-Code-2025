7b688a49756f2132296ceb36ca5dd20d91db38ae
Arik Zillhardt <arik_zillhardt@me.com>
2025-12-04 18:55:01 +0100

    Day 02 part One Solution

--- Advent Of Code/Day 02/Day02.cs
+++ Advent Of Code/Day 02/Day02.cs
@@ -8,9 +8,39 @@ namespace Advent_Of_Code.Day_02
 
         public void Run()
         {
-            var _rotations = ReadFile(_inputPath);
-            Console.WriteLine("Input lines: " + _rotations.Count);
-    
+            var _ids = ReadFile(_inputPath);
+            Console.WriteLine("Input lines: " + _ids.Count);
+
+            long invalidIds = GetInvalidIds(_ids);
+            Console.WriteLine($"Invalid IDs: {invalidIds}");
+        }
+
+        private long GetInvalidIds(List<string> ids)
+        {
+            long invalid = 0;
+
+            foreach(string id in ids)
+            {
+                long id1 = long.Parse(id.Split("-")[0]);
+                long id2 = long.Parse(id.Split("-")[1]);
+
+                for (long i = id1; i <= id2; i++)
+                {
+                    string idString = i.ToString();
+
+                    if (idString[0] == '0') continue;
+
+                    string idString1 = idString.Substring(0, idString.Length / 2);
+                    string idString2 = idString.Substring(idString.Length / 2);
+
+                    if (idString1.Equals(idString2))
+                    {
+                        invalid += i;
+                    }
+                }
+            }
+
+            return invalid;
         }
 
         private static List<string> ReadFile(string fileName)
@@ -21,8 +51,10 @@ namespace Advent_Of_Code.Day_02
                 return new List<string>();
             }
 
-            var lines = File.ReadAllLines(fileName);
-            return lines.ToList();
+            var content = File.ReadAllText(fileName);
+            var parts = content.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            return parts.ToList();
         }
     }
 }
