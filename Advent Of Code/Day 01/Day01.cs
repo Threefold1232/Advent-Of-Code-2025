d1adc5ec6f6354882bad49c4d0d1760f009c0bd6
Arik Zillhardt <arik_zillhardt@me.com>
2025-12-04 17:12:08 +0100

    Part Two Day One

--- Advent Of Code/Day 01/Day01.cs
+++ Advent Of Code/Day 01/Day01.cs
@@ -4,7 +4,7 @@ namespace Advent_Of_Code.Day_01
 {
     public class Day01
     {
-        private int _dial = 50;
+        private int _dialStart = 50;
 
         private string _inputPath = Path.Combine(AppContext.BaseDirectory, "Day 01", "input.txt");
 
@@ -12,8 +12,10 @@ namespace Advent_Of_Code.Day_01
         {
             var _rotations = ReadFile(_inputPath);
             Console.WriteLine("Input lines: " + _rotations.Count);
-            int answer = GetPassword(_rotations);
-            Console.WriteLine($"The Answer is: {answer}");
+            int answerPartOne = GetPassword(_rotations);
+            Console.WriteLine($"The Answer for Part One is: {answerPartOne}");
+            int answerPartTwo = GetNewPassword(_rotations);
+            Console.WriteLine($"The Answer for Part Two is: {answerPartTwo}");
         }
 
         private static List<string> ReadFile(string fileName)
@@ -31,6 +33,7 @@ namespace Advent_Of_Code.Day_01
         private int GetPassword(List<string> rotations)
         {
             int _zeros = 0;
+            int dial = _dialStart;
 
             foreach (string rotation in rotations)
             {
@@ -40,14 +43,14 @@ namespace Advent_Of_Code.Day_01
 
                 if (direction == 'L')
                 {
-                    _dial = (_dial - amount) % 100;
-                    if (_dial == 0) _zeros++;
+                    dial = (dial - amount) % 100;
+                    if (dial == 0) _zeros++;
                     continue;
                 } 
                 else if (direction == 'R')
                 {
-                    _dial = (_dial + amount) % 100;
-                    if (_dial == 0) _zeros++;
+                    dial = (dial + amount) % 100;
+                    if (dial == 0) _zeros++;
                     continue;
                 }
             }
@@ -55,5 +58,38 @@ namespace Advent_Of_Code.Day_01
             return _zeros;
         }
 
+        private int GetNewPassword(List<string> rotations)
+        {
+            int clicks = 0;
+            int dial = _dialStart;
+
+            foreach (string rotation in rotations)
+            {
+                char direction = char.Parse(rotation.Substring(0, 1));
+                int amount = int.Parse(rotation.Substring(1, rotation.Length - 1));
+
+
+                if (direction == 'L')
+                {
+                    int newDial = dial - amount;
+                    int oldFloor = (int)Math.Floor((dial - 1) / 100.0);
+                    int newFloor = (int)Math.Floor((newDial - 1) / 100.0);
+                    clicks += Math.Abs(oldFloor - newFloor);
+                    dial = newDial;
+                    continue;
+                } 
+                else if (direction == 'R')
+                {
+                    int newDial = dial + amount;
+                    int oldFloor = (int)Math.Floor(dial /100.0);
+                    int newFloor = (int)Math.Floor(newDial / 100.0);
+                    clicks += Math.Abs(oldFloor - newFloor);
+                    dial = newDial;
+                    continue;
+                }
+            }
+
+            return clicks;
+        }
     }
 }
