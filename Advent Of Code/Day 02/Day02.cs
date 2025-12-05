25afdea100cf37bbbea7107de488d4902282cb95
Arik Zillhardt <arik_zillhardt@me.com>
2025-12-05 07:40:43 +0100

    Day 02 Part Two Solution

--- Advent Of Code/Day 02/Day02.cs
+++ Advent Of Code/Day 02/Day02.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.Win32.SafeHandles;
 
 namespace Advent_Of_Code.Day_02
 {
@@ -13,13 +14,16 @@ namespace Advent_Of_Code.Day_02
 
             long invalidIds = GetInvalidIds(_ids);
             Console.WriteLine($"Invalid IDs: {invalidIds}");
+
+            long invalidIdsPartTwo = GetNewInvalidIds(_ids);
+            Console.WriteLine($"Invalid IDs Part 2: {invalidIdsPartTwo}");
         }
 
         private long GetInvalidIds(List<string> ids)
         {
             long invalid = 0;
 
-            foreach(string id in ids)
+            foreach (string id in ids)
             {
                 long id1 = long.Parse(id.Split("-")[0]);
                 long id2 = long.Parse(id.Split("-")[1]);
@@ -36,6 +40,66 @@ namespace Advent_Of_Code.Day_02
                     if (idString1.Equals(idString2))
                     {
                         invalid += i;
+                    }
+                }
+            }
+
+            return invalid;
+        }
+
+        List<int> numberSequence = new List<int>();
+        private long GetNewInvalidIds(List<string> ids)
+        {
+            long invalid = 0;
+
+            foreach (string id in ids)
+            {
+                long id1 = long.Parse(id.Split("-")[0]);
+                long id2 = long.Parse(id.Split("-")[1]);
+
+                for (long i = id1; i <= id2; i++)
+                {
+                    string idString = i.ToString();
+                    numberSequence.Clear();
+
+                    if (idString[0] == '0') continue;
+
+                    for (int j = 0; j < idString.Length; j++)
+                    {
+                        int idNumber = idString[j];
+                        if (!numberSequence.Contains(idNumber))
+                        {
+                            numberSequence.Add(idNumber);
+                            continue;
+                        }
+                        else
+                        { 
+                            try
+                            {                                
+                                bool matches = true;
+                                for (int a = j; a < idString.Length; a += j)
+                                {
+                                    string sequence1 = idString.Substring(a, j);
+                                    string sequence2 = idString.Substring(a - j, j);
+
+                                    if (!sequence1.Equals(sequence2))
+                                    {
+                                        matches = false;
+                                        break;
+                                    } 
+                                }
+
+                                if (matches)
+                                {
+                                    invalid += long.Parse(idString);
+                                    break;
+                                } 
+                            }
+                            catch (Exception)
+                            {
+                                break;
+                            }
+                        }
                     }
                 }
             }
