7b73696734b0b50d5d0c7d9a1f4f0170bb594c11
Arik Zillhardt <arik_zillhardt@me.com>
2025-12-05 17:52:07 +0100

    Day 03 Part One

--- Advent Of Code/Day 03/Day03.cs
+++ Advent Of Code/Day 03/Day03.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using Microsoft.Win32.SafeHandles;
 
 namespace Advent_Of_Code.Day_03
@@ -11,9 +12,46 @@ namespace Advent_Of_Code.Day_03
         {
             var _batteries = ReadFile(_inputPath);
             Console.WriteLine("Input lines: " + _batteries.Count);
+            int joltage = GetJoltage(_batteries);
+            Console.WriteLine($"Total Joltage Part One: {joltage}");
         }
 
-        
+        private int GetJoltage(List<string> batteries)
+        {
+            List<int> numbers = new List<int>();
+            int totalJoltage = 0;
+
+            foreach (string battery in batteries)
+            {
+                numbers.Clear();
+                string number1 = "";
+                string number2 = "";
+
+                int max1 = 0;
+
+                for (int i = 0; i < battery.Length - 1; i++)
+                {
+                    string first = battery[i].ToString();
+                    for (int j = i + 1; j < battery.Length; j++)
+                    {
+                        string second = battery[j].ToString();
+                        int candidate = int.Parse(first + second);
+                        if (candidate > max1)
+                        {
+                            max1 = candidate;
+                            number1 = first.ToString();
+                            number2 = second.ToString();
+                        }
+                    }
+                }
+
+                string joltageString = number1 + number2;
+                int joltage = int.Parse(joltageString);
+                totalJoltage += joltage;
+            }
+            return totalJoltage;
+        }
+
 
         private static List<string> ReadFile(string fileName)
         {
